c28d93cb6589fa088ee816177ae366c826d0cbd8
siddhesh <talktosiddhesh.shinde@gmail.com>
2026-01-30 16:51:17 +0530

    llvm pass fn

--- practice/Hello.cpp
+++ practice/Hello.cpp
@@ -1,6 +1,7 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Plugins/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/BasicBlock.h"
 
 using namespace llvm;
 
@@ -9,8 +10,35 @@ namespace
 
     void visitor(Function &F)
     {
-        errs() << "Hello from: " << F.getName() << "\n";
-        errs() << " number of arguments: " << F.arg_size() << "\n";
+        StringRef name = F.getName();
+        errs() << "\nProcessing Function: \n"
+               << name << "\n";
+        for (const BasicBlock &b : F)
+        {
+            for (const Instruction &Iobj : b)
+            {
+                const Instruction *I = &(Iobj);
+                I->print(outs());
+                if (auto bop = dyn_cast<BinaryOperator>(I))
+                {
+                    auto op0 = bop->getOperand(0);
+                    auto op1 = bop->getOperand(1);
+                    unsigned op = bop->getOpcode();
+                    if (op == Instruction::Add)
+                    {
+                        errs() << "\n Add instruction found: \n";
+                        bop->print(outs());
+                        errs() << "\ninput 0 : \n";
+                        op0->print(outs());
+                        errs() << "\ninput 1 : \n";
+                        op1->print(outs());
+                        errs() << "\n";
+                    }
+                }
+            }
+        }
+        // errs() << "Hello from: " << F.getName() << "\n";
+        // errs() << " number of arguments: " << F.arg_size() << "\n";
     }
 
     struct Hello : PassInfoMixin<Hello>
@@ -19,7 +47,7 @@ namespace
         PreservedAnalyses run(Function &F, FunctionAnalysisManager &)
         {
             visitor(F);
-            return llvm::PreservedAnalyses::all();
+            return PreservedAnalyses::all();
         }
     };
 }
